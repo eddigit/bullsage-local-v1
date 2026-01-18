6ccec349e7e3700e2272992923aca07177b60371
Gilles Korzec <gilleskorzec@gmail.com>
2026-01-18 16:15:31 +0100

    chore: Mise à jour buildInfo avec hash de commit réel

--- frontend/src/buildInfo.js
+++ frontend/src/buildInfo.js
@@ -4,9 +4,9 @@
  */
 
 export const BUILD_INFO = {
-  commitHash: "d7e8f9a",
+  commitHash: "04be2b6",
   commitDate: "2026-01-18",
-  commitTime: "14:45:00",
+  commitTime: "14:50:00",
   version: "1.2.0",
-  lastUpdate: "18 janvier 2026 à 14:45"
+  lastUpdate: "18 janvier 2026 à 14:50"
 };
